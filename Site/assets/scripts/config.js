df2c2a463aa7bf6c0025c82dc1caf670385fcc88
Felipe <60966475+fevieira27@users.noreply.github.com>
2022-02-13 21:20:07 +0000

    Add files via upload

---
+++ Site/assets/scripts/config.js
@@ -0,0 +1,14 @@
+DataConnectors = [
+];
+
+var PageFabricConfig = {
+disqusId : "",
+googleAnalyticsId : "G-YHK0E9JXFB",
+globalSearchMode : "Webservice",
+globalSearchWebServiceURL : "",
+globalSearchLanguageFiles:[
+{
+ lang: "en", 
+ url: "/en.json"
+}]
+};
